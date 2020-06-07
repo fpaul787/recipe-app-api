43d77fb0501ff380b24ed341fc57ad877060c1f2
Frantz <fpaul787@gmail.com>
2020-06-06 21:28:55 -0400

    added normailize email feature

--- app/core/models.py
+++ app/core/models.py
@@ -9,7 +9,7 @@ class UserManager(BaseUserManager):
         """
         Create and saves a new user
         """
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
